893879b1c63630ffb4419b8a53427a2579a26186
dmj@mac <manjiang.ding@gmail.com>
2020-06-30 22:47:49 +0800

    add two program with twisted

---
+++ Network_Programming/tsTclntTW.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# 这是时间戳TCP客户端，是从一个Twisted的角度来写的。
+
+from twisted.internet import protocol, reactor
+
+HOST = 'localhost'
+PORT = 21567
+
+class TSClntProtocol(protocol.Protocol):
+    def sendData(self):
+        data = input('> ')
+        if data:
+            print('...sending %s...' % data)
+            self.transport.write(bytes(data,'utf-8'))
+        else:
+            self.transport.loseConnection()
+
+    def connectionMade(self):
+        self.sendData()
+
+    def dataReceived(self, data):
+        # print(type(data))
+        # data_s = data.decode()
+        # print(type(data_s))
+        print(data.decode())
+        self.sendData()
+
+class TSClntFactory(protocol.ClientFactory):
+    protocol = TSClntProtocol
+    clientConnectionLost = clientConnectionFailed = lambda self, connector, reason: reactor.stop()
+
+reactor.connectTCP(HOST, PORT, TSClntFactory())
+reactor.run()
