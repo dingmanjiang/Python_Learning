04e3c27ec531c491c597b4afcd59092cead22e38
dmj@samsung <manjiang.ding@gmail.com>
2020-07-17 17:47:08 +0800

    go on

--- RegularExpression/myRE.py
+++ RegularExpression/myRE.py
@@ -2,31 +2,50 @@
 
 import re
 
-# 中国手机号(还没把前后文的隔离考虑进去）
-re_ChinesePhoneNumber = r'(0086|\+86)?[-\s]?1[3456789]\d[-\s]?\d{4}[-\s]?\d{4}'
+# -----------------------------------------------------------------------------
+# 								常用的正则表达式
+# -----------------------------------------------------------------------------
+# 使用方法：
+# 1、所有变量都以re作为前缀，每个子类下以下划线分割
+
+# -------------------------------------------
+# 子类：电话号码(rege_phone)
+# -------------------------------------------
+
+# 中国手机号
+rege_phone_ChineseMobile = r'''(?x)						#多行注释模式
+							 (?<!\d)					#号码之前数字开头
+							 (?:(?:0086|\+86)[-\s]?)?	#有或没有国际区号和间隔
+							 1[3456789]\d				#手机号前三位
+							 [-\s]?						#手机号之间的间隔
+							 \d{4}						#手机号中间四位
+							 [-\s]?						#手机号之间的间隔
+							 \d{4}						#手机号后四位
+							 (?!\d)						#号码之后不能是数字
+							'''
+
+# -------------------------------------------
+# 子类：网络地址(rege_net)
+# -------------------------------------------
 
 # url_protocol
-# 判断http://、https://、ftp://
-re_UrlProtocol = r'(https?|ftp):\/\/'
+rege_net_UrlProtocol = r'(https?|ftp):\/\/' #判断http://、https://、ftp://
 
 
 
-text = '+86-131-5261-4238 13152613238'
-#pattern = re.compile(re_ChinesePhoneNumber)
+text = '+86-131-5261-4238 13152613238 132998398234'
+#pattern = re.compile(rege_phone_ChineseMobile)
 #print(pattern.search(text))
 #print(pattern.match(text))
 #print(pattern.findall(text))
-match = re.search(re_ChinesePhoneNumber,text)
+match = re.search(rege_phone_ChineseMobile,text)
 if match:
     print(match.group(0))
 
-matchfindall = re.findall(re_ChinesePhoneNumber,text)
+matchfindall = re.findall(rege_phone_ChineseMobile,text)
 if match:
     print(matchfindall)
 
-for m in re.finditer(re_ChinesePhoneNumber, text):
+for m in re.finditer(rege_phone_ChineseMobile, text):
     if m:
         print(m.group(0))
-
-ls = re.findall(r'(0086|\+86)?[-\s]?1[3456789]\d[-\s]?\d{4}[-\s]?\d{4}','13152613328  +8613813831381')
-print(ls)
