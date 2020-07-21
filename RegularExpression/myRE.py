57bfc120988f6cfe309b3e1390f38e1aeb656051
dmj@samsung <manjiang.ding@gmail.com>
2020-07-21 17:29:38 +0800

    Go on in chapter 1

--- RegularExpression/myRE.py
+++ RegularExpression/myRE.py
@@ -15,14 +15,14 @@ import re
 
 # 中国手机号
 rege_phone_ChineseMobile = r'''(?x)						#多行注释模式
-							 (?<!\d)					#号码之前数字开头
-							 (?:(?:0086|\+86)[-\s]?)?	#有或没有国际区号和间隔
-							 1[3456789]\d				#手机号前三位
-							 [-\s]?						#手机号之间的间隔
-							 \d{4}						#手机号中间四位
-							 [-\s]?						#手机号之间的间隔
-							 \d{4}						#手机号后四位
-							 (?!\d)						#号码之后不能是数字
+							(?<!\d)						#号码之前数字开头
+							(?:(?:0086|\+86)[-\s]?)?	#有或没有国际区号和间隔
+							1[3456789]\d				#手机号前三位
+							[-\s]?						#手机号之间的间隔
+							\d{4}						#手机号中间四位
+							[-\s]?						#手机号之间的间隔
+							\d{4}						#手机号后四位
+							(?!\d)						#号码之后不能是数字
 							'''
 
 # -------------------------------------------
@@ -30,14 +30,14 @@ rege_phone_ChineseMobile = r'''(?x)						#多行注释模式
 # -------------------------------------------
 
 # url_protocol
-rege_net_UrlProtocol = r'(https?|ftp):\/\/' #判断http://、https://、ftp://
+rege_net_UrlProtocol = r'(https?|ftp):\/\/'
+# 判断http://、https://、ftp://
 
 
 print(re.findall('\\\\', 'a*b+c?\\d123d\\'))
 
 
-
-#text = '+86-131-5261-4238 13152613238 132998398234'
+# text = '+86-131-5261-4238 13152613238 132998398234'
 # match = re.search(rege_phone_ChineseMobile,text)
 # if match:
 #     print(match.group(0))
@@ -49,4 +49,3 @@ print(re.findall('\\\\', 'a*b+c?\\d123d\\'))
 # for m in re.finditer(rege_phone_ChineseMobile, text):
 #     if m:
 #         print(m.group(0))
-
